d7f0f7b6781d9b818a16b2b83b0b8b3cb719a34f
Jesus Morales <gsus@Jesuss-MacBook-Pro.local>
2026-04-21 22:08:51 -0600

    feat:Agregar logo y cambiar el icono de cuentas

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -18,6 +18,7 @@ import BuildIcon from '@mui/icons-material/Build';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 
 export const DRAWER_WIDTH = 240;
 export const DRAWER_WIDTH_COLLAPSED = 64;
@@ -49,7 +50,7 @@ const NAV_ITEMS: NavItem[] = [
   },
   {
     label: 'Cuentas',
-    icon: <PeopleIcon />,
+    icon: <AccountBalanceIcon />,
     href: '/cuentas',
     },
   {
